ce13f1ebe6f66db041adccbd55ead7175cfe4841
AnthonyFic-code <76977714+AnthonyFic-code@users.noreply.github.com>
2021-01-30 21:14:19 -0500

    initial commit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var username = "Angie";
 var password = "pass1";
 var myButton = document.getElementById("login");
 myButton.addEventListener("click", logIn);
-var myButton2 = document.getElementById("");
-myButton2.addEventListener("click", logIn);
+var myButton2 = document.getElementById("submit");
+myButton2.addEventListener("click", see);
 function logIn() {
   var ident = document.getElementById("user").value;
   var ident2 = document.getElementById("pass").value;
@@ -22,6 +22,15 @@ function updateScreen() {
 }
 
 function updateScreen2() {
+  let incorrect = document.getElementById("incorrect");
+  incorrect.innerText = "Invalid Login";
+}
+
+function see() {
+  var messageName = document.getElementById("first_name").value;
+  var messageLastName = document.getElementById("last_name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
   let incorrect = document.getElementById("incorrect");
   incorrect.innerText = "Invalid Login";
 }
